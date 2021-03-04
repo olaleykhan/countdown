1862c23144e1fcd1c2f0152b8172a57a715cd13d
olaleykhan <realjoy9@gmail.com>
2021-03-04 15:23:26 +0100

    fitst commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Wp"/> -->
+  <heading headingText="WE'RE LAUNCHING SOON" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+import Heading from './components/Heading.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Heading,
   }
 }
 </script>
---
+++ src/components/Heading.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>{{headingText}} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Heading",
+
+    props: {
+        headingText: String
+    }
+}
+</script>
